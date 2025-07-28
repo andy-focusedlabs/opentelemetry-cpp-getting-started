b6f75b56440d6e5dd7f14d77e5a05580a3eaf0d1
Andy Ennamorato <andy.ennamorato+github@focusedlabs.io>
2025-07-28 16:10:24 -0600

    capturing pre-otel setup

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include "oatpp/web/server/HttpConnectionHandler.hpp"
+#include "oatpp/network/Server.hpp"
+#include "oatpp/network/tcp/server/ConnectionProvider.hpp"
+#include <cstdlib>
+#include <ctime>
+#include <string>
+
+using namespace std;
+
+class Handler : public oatpp::web::server::HttpRequestHandler {
+public:
+  shared_ptr<OutgoingResponse> handle(const shared_ptr<IncomingRequest>& request) override {
+    int low = 1;
+    int high = 7;
+    int random = rand() % (high - low) + low;
+    // Convert a std::string to oatpp::String
+    const string response = to_string(random);
+    return ResponseFactory::createResponse(Status::CODE_200, response.c_str());
+  }
+};
+
+void run() {
+  auto router = oatpp::web::server::HttpRouter::createShared();
+  router->route("GET", "/rolldice", std::make_shared<Handler>());
+  auto connectionHandler = oatpp::web::server::HttpConnectionHandler::createShared(router);
+  auto connectionProvider = oatpp::network::tcp::server::ConnectionProvider::createShared({"localhost", 8080, oatpp::network::Address::IP_4});
+  oatpp::network::Server server(connectionProvider, connectionHandler);
+  OATPP_LOGI("Dice Server", "Server running on port %s", static_cast<const char*>(connectionProvider->getProperty("port").getData()));
+  server.run();
+}
+
+int main() {
+  oatpp::base::Environment::init();
+  srand((int)time(0));
+  run();
+  oatpp::base::Environment::destroy();
+  return 0;
+}
+
