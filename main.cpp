0f0496fbc12d6bfe73b5a6767a3929c23a8ab670
Andy Ennamorato <andy.ennamorato+github@focusedlabs.io>
2025-07-28 16:35:56 -0600

    basic otel setup
    * emits traces to stdout

--- main.cpp
+++ main.cpp
@@ -1,23 +1,57 @@
 #include "oatpp/web/server/HttpConnectionHandler.hpp"
 #include "oatpp/network/Server.hpp"
 #include "oatpp/network/tcp/server/ConnectionProvider.hpp"
-#include "oatpp/core/base/Environment.hpp"
+
+#include "opentelemetry/exporters/ostream/span_exporter_factory.h"
+#include "opentelemetry/sdk/trace/exporter.h"
+#include "opentelemetry/sdk/trace/processor.h"
+#include "opentelemetry/sdk/trace/simple_processor_factory.h"
+#include "opentelemetry/sdk/trace/tracer_provider_factory.h"
+#include "opentelemetry/trace/provider.h"
+
 #include <cstdlib>
 #include <ctime>
 #include <string>
-#include <fstream>
 
 using namespace std;
+namespace resource       = opentelemetry::sdk::resource;
+namespace trace_api = opentelemetry::trace;
+namespace trace_sdk = opentelemetry::sdk::trace;
+namespace trace_exporter = opentelemetry::exporter::trace;
+
+namespace {
+  void InitTracer() {
+    auto exporter  = trace_exporter::OStreamSpanExporterFactory::Create();
+    auto processor = trace_sdk::SimpleSpanProcessorFactory::Create(std::move(exporter));
+    resource::ResourceAttributes attributes = {{"service.name", "example_cpp_service"},
+                                             {"version", static_cast<uint32_t>(1)}};
+    auto resource                           = resource::Resource::Create(attributes);
+    std::shared_ptr<opentelemetry::trace::TracerProvider> provider =
+      trace_sdk::TracerProviderFactory::Create(std::move(processor), resource);
+
+    //set the global trace provider
+    trace_api::Provider::SetTracerProvider(provider);
+  }
+  void CleanupTracer() {
+    std::shared_ptr<opentelemetry::trace::TracerProvider> none;
+    trace_api::Provider::SetTracerProvider(none);
+  }
+
+}
 
 class Handler : public oatpp::web::server::HttpRequestHandler {
 public:
   shared_ptr<OutgoingResponse> handle(const shared_ptr<IncomingRequest>& request) override {
+    auto tracer = opentelemetry::trace::Provider::GetTracerProvider()->GetTracer("my-app-tracer");
+    auto span = tracer->StartSpan("RollDiceServer");
     int low = 1;
     int high = 7;
     int random = rand() % (high - low) + low;
-    OATPP_LOGI("DiceRoll", "Generated dice roll: %d", random);
     // Convert a std::string to oatpp::String
     const string response = to_string(random);
+    OATPP_LOGI("DiceRoll", "Generated dice roll: %d", random);
+
+    span->End();
     return ResponseFactory::createResponse(Status::CODE_200, response.c_str());
   }
 };
@@ -34,9 +68,10 @@ void run() {
 
 int main() {
   oatpp::base::Environment::init();
+  InitTracer();
   srand((int)time(0));
   run();
   oatpp::base::Environment::destroy();
+  CleanupTracer();
   return 0;
 }
-
